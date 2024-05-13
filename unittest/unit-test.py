10602bd2a67a141cd6b18e7ed4f3987916060359
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-13 15:19:35 +0200

    unittest passed #3

---
+++ unittest/unit-test.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import unittest
+
+sys.path.append("src/")
+
+from dataloader import Loader
+from utils import load, config
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.train_dataloader = load(
+            filename=os.path.join(
+                config()["path"]["processed_path"], "train_dataloader.pkl"
+            )
+        )
+        self.test_dataloader = load(
+            filename=os.path.join(
+                config()["path"]["processed_path"], "test_dataloader.pkl"
+            )
+        )
+
+    def test_total_train_data(self):
+        self.assertEqual(sum(X.size(0) for X, _ in self.train_dataloader), 18)
+
+    def test_total_test_data(self):
+        self.assertEqual(sum(X.size(0) for X, _ in self.test_dataloader), 7)
+
+    def test_train_data_shape(self):
+        self.assertEqual(self.train_dataloader.dataset[0][0].shape, (3, 256, 256))
+
+    def test_test_data_shape(self):
+        self.assertEqual(self.test_dataloader.dataset[0][0].shape, (3, 256, 256))
+
+    def test_total_data(self):
+        total_data = sum(X.size(0) for X, _ in self.train_dataloader) + sum(
+            X.size(0) for X, _ in self.test_dataloader
+        )
+        self.assertEqual(total_data, 25)
+
+
+if __name__ == "__main__":
+    unittest.main()
