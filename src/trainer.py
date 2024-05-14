46a53423e67fb6cbf8fe3fc6426fea7d74254b99
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-14 17:55:34 +0200

    trainer prototype is added in src

--- src/trainer.py
+++ src/trainer.py
@@ -1,10 +1,12 @@
 import sys
 import os
 
+from torch.optim.lr_scheduler import StepLR
+
 sys.path.append("src/")
 
 from helper import helpers
-from utils import config, device_init, load, dump
+from utils import config, device_init, weight_init, load, dump
 
 
 class Trainer:
@@ -53,4 +55,50 @@ class Trainer:
         self.grad_penalty = self.init["grad_penalty"]
 
         if self.is_weight_init:
-            pass
+            self.netGX_toY.apply(weight_init)
+            self.netGY_toX.apply(weight_init)
+
+            self.netDX.apply(weight_init)
+            self.netDY.apply(weight_init)
+
+        if self.lr_scheduler:
+            self.schedulerG = StepLR(
+                optimizer=self.optimizerG,
+                step_size=20,
+                gamma=0.5,
+            )
+            self.schedulerD_X = StepLR(
+                optimizer=self.optimizerD_X,
+                step_size=20,
+                gamma=0.5,
+            )
+            self.schedulerD_Y = StepLR(
+                optimizer=self.optimizerD_Y,
+                step_size=20,
+                gamma=0.5,
+            )
+
+        print(self.netGX_toY)
+        print(self.netGY_toX)
+        print(self.netDX)
+        print(self.netDY)
+        print(self.optimizerG)
+        print(self.optimizerD_X)
+        print(self.optimizerD_Y)
+        print(self.train_dataloader)
+        print(self.test_dataloader)
+        print(self.cycle_loss)
+        print(self.grad_penalty)
+
+
+if __name__ == "__main__":
+    trainer = Trainer(
+        in_channels=3,
+        epochs=1,
+        lr=0.0002,
+        device="mps",
+        adam=True,
+        SGD=False,
+        is_weight_init=True,
+        lr_scheduler=False,
+    )
