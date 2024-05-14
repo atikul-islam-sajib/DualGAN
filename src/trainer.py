4400b2894b8e8ec2698a805afa4423d9394fa866
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-05-14 18:53:13 +0200

    trainer code is updated with saved_checkpoints

--- src/trainer.py
+++ src/trainer.py
@@ -18,19 +18,23 @@ class Trainer:
         in_channels=3,
         epochs=500,
         lr=2e-3,
+        num_critics=5,
         device="cuda",
         adam=True,
         SGD=False,
         is_weight_init=True,
+        is_display=True,
         lr_scheduler=False,
     ):
         self.in_channels = in_channels
         self.epochs = epochs
         self.lr = lr
+        self.num_critics = num_critics
         self.device = device
         self.adam = adam
         self.SGD = SGD
         self.is_weight_init = is_weight_init
+        self.is_display = is_display
         self.lr_scheduler = lr_scheduler
 
         self.device = device_init(
@@ -84,6 +88,22 @@ class Trainer:
                 step_size=20,
                 gamma=0.5,
             )
+        try:
+            self.config_files = config()
+        except Exception as e:
+            print("An Error occur in the code:".capitalize(), e)
+
+        else:
+            self.netG_XtoY_path = self.config_files["path"]["netG_XtoY_path"]
+            self.netG_YtoX_path = self.config_files["path"]["netG_YtoX_path"]
+            self.best_model_path = self.config_files["path"]["best_model_path"]
+            self.train_results = self.config_files["path"]["train_results"]
+
+        self.loss = float("inf")
+
+        self.total_netG_loss = []
+        self.total_netD_X_loss = []
+        self.total_netD_Y_loss = []
 
     def update_netG(self, **kwargs):
         self.optimizerG.zero_grad()
@@ -145,6 +165,60 @@ class Trainer:
 
         return netD_Y_loss.item()
 
+    def show_progress(self, **kwargs):
+        if self.is_display:
+            print(
+                "Epochs - [{}/{}] - netG_loss: {} - netD_X_loss: {} - netD_Y_loss: {}".format(
+                    kwargs["epoch"] + 1,
+                    self.epochs,
+                    np.mean(kwargs["netG_loss"]),
+                    np.mean(kwargs["netD_X_loss"]),
+                    np.mean(kwargs["netD_Y_loss"]),
+                )
+            )
+        else:
+            print(
+                "Epochs - [{}/{}] is completed".capitalize().format(
+                    kwargs["epochs"],
+                    self.epochs,
+                )
+            )
+
+    def saved_checkpoints(self, **kwargs):
+        if (
+            os.path.exists(self.netG_XtoY_path)
+            and os.path.exists(self.netG_YtoX_path)
+            and os.path.exists(self.best_model_path)
+        ):
+            torch.save(
+                self.netGX_toY.state_dict(),
+                os.path.join(
+                    self.netG_XtoY_path, "netG_XtoY{}.pth".format(kwargs["epoch"])
+                ),
+            )
+            torch.save(
+                self.netGY_toX.state_dict(),
+                os.path.join(
+                    self.netG_YtoX_path, "netG_YtoX{}.pth".format(kwargs["epoch"])
+                ),
+            )
+
+            if self.loss > kwargs["netG_loss"]:
+                self.loss = kwargs["netG_loss"]
+
+                torch.save(
+                    {
+                        "netG_XtoY": self.netGX_toY.state_dict(),
+                        "netG_YtoX": self.netGY_toX.state_dict(),
+                        "netG_loss": kwargs["netG_loss"],
+                        "epoch": kwargs["epoch"],
+                    },
+                    os.path.join(self.best_model_path, "best_model.pth"),
+                )
+
+        else:
+            raise Exception("Cannot be saved the model")
+
     def train(self):
         for epoch in tqdm(range(self.epochs)):
             netG_loss = []
@@ -158,18 +232,17 @@ class Trainer:
                 netD_X_loss.append(self.update_netD_X(X=X, y=y))
                 netD_Y_loss.append(self.update_netD_Y(X=X, y=y))
 
-                if idx % 5:
+                if (idx + 1) % self.num_critics:
                     netG_loss.append(self.update_netG(X=X, y=y))
 
-            print(
-                "Epochs - [{}/{}] - netG_loss: {} - netD_X_loss: {} - netD_Y_loss: {}".format(
-                    epoch + 1,
-                    self.epochs,
-                    np.mean(netG_loss),
-                    np.mean(netD_X_loss),
-                    np.mean(netD_Y_loss),
-                )
+            self.show_progress(
+                epoch=epoch + 1,
+                netG_loss=netG_loss,
+                netD_X_loss=netD_X_loss,
+                netD_Y_loss=netD_Y_loss,
             )
+
+            self.saved_checkpoints(epoch=epoch + 1, netG_loss=np.mean(netG_loss))
 
 
 if __name__ == "__main__":
